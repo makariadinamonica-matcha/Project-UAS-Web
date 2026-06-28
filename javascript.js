f1d45d60e2062eb1e35f7d82cf6eb809eedd3071
Makaria Monica <makariadinamonica@itbss.ac.id>
2026-06-28 18:49:53 +0700

    Implement dark mode toggle and smooth scrolling

--- javascript.js
+++ javascript.js
@@ -1,1 +1,45 @@
 
+
+window.onload = function(){
+
+    alert("👋 Selamat Datang di Website GlowLab!");
+
+}
+
+const darkBtn = document.getElementById("darkModeBtn");
+
+darkBtn.addEventListener("click", function(){
+
+    document.body.classList.toggle("bg-dark");
+    document.body.classList.toggle("text-white");
+
+    let card = document.querySelectorAll(".card");
+
+    card.forEach(function(item){
+
+        item.classList.toggle("bg-secondary");
+        item.classList.toggle("text-white");
+
+    });
+
+});
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor=>{
+
+    anchor.addEventListener("click",function(e){
+
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+
+            behavior:"smooth"
+
+        });
+
+    });
+
+});
+
+
+console.log("GlowLab berhasil dijalankan.");
